30ca417136fac77204e4df62040de6cdc114dffe
Navin Chikkodi <73332645+og-nav@users.noreply.github.com>
2023-01-29 02:25:11 -0600

    MrBeastTwitterBot

--- main.py
+++ main.py
@@ -2,8 +2,21 @@ import tweepy
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-API_KEY = os.getenv('API_KEY')
-API_SECRET = os.getenv('API_SECRET')
-CLIENT_ID = os.getenv('CLIENT_ID')
-CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+def post_tweet():
+  load_dotenv()
+  API_KEY = os.getenv('API_KEY')
+  API_SECRET = os.getenv('API_SECRET')
+  ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
+  ACCESS_TOKEN_SECRET = os.getenv('ACCESS_TOKEN_SECRET')
+
+  auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
+  auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+  api = tweepy.API(auth)
+
+  tweets = api.user_timeline(screen_name='trustfundpro')
+  yesterday = int(tweets[0].text.split(' ')[1])
+  new_tweet = f'Day {yesterday + 1} of asking @MrBeast for $10,000'
+  media = api.media_upload('bernie.jpg')
+  post = api.update_status(status=new_tweet, media_ids=[media.media_id])
+
+post_tweet()
